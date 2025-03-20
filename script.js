c1b9378b1c83886da2f03ce2481db9783b07d130
aryanlimbachiya <alimbachiya960@gmail.com>
2025-03-20 15:48:18 +0530

    Gym

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.onscroll = () => {
 
 const typed = new Typed('.multiple-text', {
     strings: ['Physical Fitness', 'Weight Gain', 'Strength Training', 'Fat Lose', 'Weight Lifting', 'Running'],
-    typeSpeed: 60,
+    typeSpeed: 10,
     backSpeed: 60,
     backDelay: 1000,
     loop: true,
